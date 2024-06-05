2bed5b0c48aefd3eeb8d3839073e2efed25ffcad
Rui Teixeira <rui.teixeira@minderacodeacademy.com>
2024-06-05 08:31:20 +0100

    io exercise - added custom exceptions. new class ioFunctions have all the functions, main is now cleaner with only the menu

--- src/ioexercise/Main.java
+++ src/ioexercise/Main.java
@@ -1,248 +1,65 @@
 package ioexercise;
 
+import ioexercise.IoFunctions.IoFunctions;
+
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String input = null;
-        boolean exitProgram = false;
-        while (!exitProgram) {
+        //boolean exitProgram = false;
+        while (true) {
             try {
                 printMainMenu();
                 System.out.print("[option]> ");
                 input = reader.readLine().toUpperCase();
-
-                MenuEnum test = MenuEnum.valueOf(input);
-                switch (test) {
+                MenuEnum choice = checkChoice(input);
+                switch (choice) {
                     case OP1A:
-                        askAndPrintUserDirToPrint();
+                        IoFunctions.listDirectory();
                         break;
                     case OP2A:
-                        askUserForFileAndPathAndSeeIfExists();
+                        IoFunctions.checkIfFileExists();
                         break;
                     case OP2B:
-                        askUserForFileAndPathAndSeeIfExistsFull();
+                        IoFunctions.checkIfFileExistsAbsolute();
                         break;
                     case OP3A:
-                        createNewFileInPath();
+                        IoFunctions.createNewFile();
                         break;
                     case OP3B:
-                        createNewFileInPathFull();
+                        IoFunctions.createNewFileAbsolute();
                         break;
                     case EXIT:
-                        exitProgram = true;
+                        return;
+                    case null:
+                        System.out.println("Invalid option.");
                         break;
                     default:
                         System.out.println("Not an option.");
                 }
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                System.out.println("Something went wrong. Please try again.\nDetails: " + e.getMessage());
             }
         }
+    }
+
+    public static MenuEnum checkChoice(String choice) {
+        for (int i = 0; i < MenuEnum.values().length; i++) {
+            if (choice.equals(MenuEnum.values()[i].getOption())) {
+                return MenuEnum.values()[i];
+            }
+        }
+        return null;
     }
 
     public static void printMainMenu() {
         System.out.println();
         System.out.printf("%s\t%s\n", "Option", "Description");
         for (int i = 0; i < MenuEnum.values().length; i++) {
-            System.out.printf("[%s]\t%s\n", MenuEnum.values()[i], MenuEnum.values()[i].getDesc());
+            System.out.printf("[%s]\t%s\n", MenuEnum.values()[i].getOption(), MenuEnum.values()[i].getDesc());
         }
         System.out.println();
-    }
-
-    public static void askAndPrintUserDirToPrint() {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String input = null;
-        try {
-            System.out.println("Please input the directory to display all files inside it: ");
-            System.out.println("Absolute and Relative paths are valid.");
-            System.out.print("[directory]> ");
-            input = reader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        File dirToList = new File(input);
-        if (!dirToList.exists()) {
-            System.out.println("Path is invalid / does not exist.");
-            return;
-        }
-
-        System.out.println("Listing files and directories from '" + input + "'");
-        for (String dir : dirToList.list()) {
-            System.out.println(dir);
-        }
-    }
-
-    public static void askUserForFileAndPathAndSeeIfExists() {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String fileName = null;
-        String path = null;
-
-        try {
-            System.out.println("Please input the file / directory to check if exists: ");
-            System.out.println("Absolute and Relative paths are valid.");
-            System.out.print("[file]> ");
-            fileName = reader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            System.out.println("Enter path. This file / directory should be at '[path]/" + fileName);
-            System.out.print("[path]> ");
-            path = reader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        path = path.concat("/");
-        File checkIfExist = new File(path.concat(fileName));
-        if (!checkIfExist.exists()) {
-            System.out.println("The path to the file is invalid / does not exist.");
-            return;
-        }
-
-        System.out.println("The file / directory exists! Final path to file: " + path.concat(fileName));
-    }
-
-    public static void askUserForFileAndPathAndSeeIfExistsFull() {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String fileName = null;
-        String path = null;
-
-        try {
-            System.out.println("Please input the directory to check if file exists: ");
-            System.out.println("Only absolute paths are valid.");
-            System.out.print("[directory]> ");
-            path = reader.readLine();
-
-            File checkIfExist = new File(path);
-            if (!checkIfExist.isAbsolute()) {
-                System.out.println("Only absolute paths are valid.");
-                return;
-            }
-
-            if (!checkIfExist.exists()) {
-                System.out.println("Path is invalid / does not exist.");
-                return;
-            }
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            System.out.println("Please input the file name to check if exists: ");
-            System.out.print(path + "/[file]> ");
-            fileName = reader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        path = path.concat("/");
-        File checkIfExist = new File(path.concat(fileName));
-        if (!checkIfExist.exists()) {
-            System.out.println("Path is invalid / does not exist.");
-            return;
-        }
-
-        System.out.println("The file exists! File path: " + path.concat(fileName));
-    }
-
-    public static void createNewFileInPath() {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String fileName = null;
-        String path = null;
-
-        try {
-            System.out.println("Please input the directory to save the file: ");
-            System.out.println("Absolute and Relative paths are valid.");
-            System.out.print("[directory]> ");
-            path = reader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            System.out.println("Please input the file name: ");
-            System.out.println("Absolute and Relative paths are valid.");
-            System.out.print(path + "/[file]> ");
-            fileName = reader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        path = path.concat("/");
-        path = path.concat(fileName);
-
-        String text = "This is a new file with this text inside.\n";
-
-        BufferedWriter writer = null;
-        try {
-            writer = new BufferedWriter(new FileWriter(path));
-            writer.write(text);
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        System.out.println("The file was made with success! File path: " + path);
-    }
-
-    public static void createNewFileInPathFull() {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String fileName = null;
-        String path = null;
-
-        try {
-            System.out.println("Please input the directory to save the file: ");
-            System.out.println("Only absolute paths are valid.");
-            System.out.print("[directory]> ");
-            path = reader.readLine();
-
-            File checkIfExist = new File(path);
-            if (!checkIfExist.isAbsolute()) {
-                System.out.println("Only absolute paths are valid.");
-                return;
-            }
-
-            if (!checkIfExist.exists()) {
-                System.out.println("Path is invalid / does not exist.");
-                return;
-            }
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            System.out.println("Please input the file name: ");
-            System.out.println("Absolute and Relative paths are valid.");
-            System.out.print(path + "/[file]> ");
-            fileName = reader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        path = path.concat("/");
-        path = path.concat(fileName);
-
-        String text = "This is a new file with this text inside.\n";
-
-        BufferedWriter writer = null;
-        try {
-            writer = new BufferedWriter(new FileWriter(path));
-            writer.write(text);
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        System.out.println("The file was made with success! File path: " + path);
     }
 }
--- src/ioexercise/MenuEnum.java
+++ src/ioexercise/MenuEnum.java
@@ -1,12 +1,12 @@
 package ioexercise;
 
 public enum MenuEnum {
-    EXIT(0, "0", "exit"),
     OP1A(1,"1", "List files of a directory"),
     OP2A(2,"2", "Check if a file / directory exists - Absolute / Relative paths"),
     OP2B(3,"3", "Check if a file / directory exists - Absolute paths"),
     OP3A(4,"5", "Create new file in a path with custom name - Absolute / Relative paths"),
-    OP3B(5,"5", "Create new file in a path with custom name - Absolute paths");
+    OP3B(5,"6", "Create new file in a path with custom name - Absolute paths"),
+    EXIT(0,"0", "Exit");
 
     private int id;
     private String option;
